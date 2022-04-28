3b0c494fdd9e2ad2673c5f62984cfa4652dfb0da
eastshine-high <eastshine.high@gmail.com>
2022-04-28 20:15:36 +0900

    feat(common): 오류는 일관성 있게 표현하여 응답한다

---
+++ app/src/main/java/com/eastshine/auction/common/advice/ControllerErrorAdvice.java
@@ -0,0 +1,79 @@
+package com.eastshine.auction.common.advice;
+
+import com.eastshine.auction.common.dto.ErrorResponse;
+import com.eastshine.auction.common.exception.EntityNotFoundException;
+import com.eastshine.auction.common.exception.ErrorCode;
+import com.eastshine.auction.common.exception.InvalidArgumentException;
+import com.eastshine.auction.common.interceptor.CommonHttpRequestInterceptor;
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.core.NestedExceptionUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+@Slf4j
+@ControllerAdvice
+public class ControllerErrorAdvice {
+    private final MessageSource messageSource;
+
+    public ControllerErrorAdvice(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    private static final List<ErrorCode> SPECIFIC_ALERT_TARGET_ERROR_CODE_LIST = Lists.newArrayList();
+
+    /**
+     * 시스템 예외 상황.
+     */
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(value = Exception.class)
+    public ErrorResponse onException(Exception e) {
+        String eventId = MDC.get(CommonHttpRequestInterceptor.HEADER_REQUEST_UUID_KEY);
+        log.error("eventId = {} ", eventId, e);
+        return ErrorResponse.of(ErrorCode.COMMON_SYSTEM_ERROR);
+    }
+
+    /**
+     * 비즈니스 로직 처리에서 발생한 예외.
+     */
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = InvalidArgumentException.class)
+    public ErrorResponse onInvalidArgumentException(InvalidArgumentException e) {
+        String eventId = MDC.get(CommonHttpRequestInterceptor.HEADER_REQUEST_UUID_KEY);
+        if (SPECIFIC_ALERT_TARGET_ERROR_CODE_LIST.contains(e.getErrorCode())) {
+            log.error("[BaseException] eventId = {}, cause = {}, errorMsg = {}", eventId, NestedExceptionUtils.getMostSpecificCause(e), NestedExceptionUtils.getMostSpecificCause(e).getMessage());
+        } else {
+            log.warn("[BaseException] eventId = {}, cause = {}, errorMsg = {}", eventId, NestedExceptionUtils.getMostSpecificCause(e), NestedExceptionUtils.getMostSpecificCause(e).getMessage());
+        }
+        return ErrorResponse.of(e.getMessage(), e.getErrorCode().name());
+    }
+
+    /**
+     * 비즈니스 로직 처리에서 발생한 예외.
+     */
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(value = EntityNotFoundException.class)
+    public ErrorResponse onEntityNotFoundException(EntityNotFoundException e) {
+        String eventId = MDC.get(CommonHttpRequestInterceptor.HEADER_REQUEST_UUID_KEY);
+        if (SPECIFIC_ALERT_TARGET_ERROR_CODE_LIST.contains(e.getErrorCode())) {
+            log.error("[BaseException] eventId = {}, cause = {}, errorMsg = {}", eventId, NestedExceptionUtils.getMostSpecificCause(e), NestedExceptionUtils.getMostSpecificCause(e).getMessage());
+        } else {
+            log.warn("[BaseException] eventId = {}, cause = {}, errorMsg = {}", eventId, NestedExceptionUtils.getMostSpecificCause(e), NestedExceptionUtils.getMostSpecificCause(e).getMessage());
+        }
+        return ErrorResponse.of(e.getMessage(), e.getErrorCode().name());
+    }
+}
