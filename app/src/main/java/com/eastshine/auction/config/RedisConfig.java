061e92b159c6222b6639fe3fff00e5dbc4f706f9
eastshine-high <eastshine.high@gmail.com>
2022-06-13 19:56:33 +0900

    feat(config): Redis를 캐시로 사용하기 위한 구성 등록

---
+++ app/src/main/java/com/eastshine/auction/config/RedisConfig.java
@@ -0,0 +1,45 @@
+package com.eastshine.auction.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@EnableCaching
+@Configuration
+public class RedisConfig {
+    public static final long CACHE_DURATION_HOURS = 6L;
+
+    @Value("${spring.redis.port}")
+    public int port;
+
+    @Value("${spring.redis.host}")
+    public String host;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory cf) {
+        RedisCacheManager.RedisCacheManagerBuilder builder = RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory());
+
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofHours(CACHE_DURATION_HOURS));
+
+        return builder
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+}
