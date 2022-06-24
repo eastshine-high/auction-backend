9dec9717a1df07d3f3b8883f48f670120aac6b91
eastshine-high <eastshine.high@gmail.com>
2022-06-24 22:51:04 +0900

    test(common): 인가 테스트를 위한 토큰 상수 추가

--- app/src/test/java/com/eastshine/auction/common/test/IntegrationTest.java
+++ app/src/test/java/com/eastshine/auction/common/test/IntegrationTest.java
@@ -1,20 +1,27 @@
 package com.eastshine.auction.common.test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@AutoConfigureRestDocs(uriScheme = "http", uriHost = "3.36.136.227", uriPort = 80)
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
+    protected static final String VALID_AUTHENTICATION = "Bearer eyJhbGciOiJIUzI1NiJ9" +
+            ".eyJ1c2VySW5mbyI6eyJpZCI6MSwiZW1haWwiOiJ0ZXN0QGdtYWlsLmNvbSIsIm5pY2tuYW1lIjoibmlja25hbWUiLCJyb2xlcyI6WyJVU0VSIiwiQURNSU4iXX19" +
+            ".2s3sfdLWmcdyT4FeXz8wzKeODyPmxkLHJSF8jmGwOPI";
+    protected static final String INVALID_AUTHENTICATION = "INVALID_TOKEN";
 
     @Autowired
     protected MockMvc mockMvc;
 
     @Autowired
     protected ObjectMapper objectMapper;
+
+    protected String createJson(Object dto) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(dto);
+    }
 }
