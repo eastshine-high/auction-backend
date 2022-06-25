7e7c728711bfd5da721f4ab9755d459b92ccfc54
eastshine-high <eastshine.high@gmail.com>
2022-06-25 20:15:36 +0900

    test(category): 디스플레이용 카테고리 조회에 대한 테스트

---
+++ app/src/test/java/com/eastshine/auction/category/web/DisplayCategoryControllerTest.java
@@ -0,0 +1,42 @@
+package com.eastshine.auction.category.web;
+
+import com.eastshine.auction.category.CategoryFactory;
+import com.eastshine.auction.common.test.RestDocsTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class DisplayCategoryControllerTest extends RestDocsTest {
+
+    @Autowired CategoryFactory categoryFactory;
+
+    @BeforeEach
+    void setUp() {
+        categoryFactory.deleteAllCategory();
+
+        categoryFactory.createCategory(101, "패션/뷰티", 1);
+        categoryFactory.createCategory(1010001, 101, "스킨케어", 1);
+        categoryFactory.createCategory(1010002, 101, "헤어/바디/미용", 2);
+        categoryFactory.createCategory(102, "디지털/가전", 2);
+        categoryFactory.createCategory(1020001, 102, "가공식품/즉석/간신", 1);
+        categoryFactory.createCategory(103, "가구/생활/자동차", 3);
+    }
+
+    @Test
+    void getDisplayCategories() throws Exception {
+        mockMvc.perform(
+                get("/api/display/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("103")))
+                .andDo(document("get-displayCategories-200"));
+    }
+}
