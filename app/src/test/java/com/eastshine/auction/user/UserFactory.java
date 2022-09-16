c230727c2eeca1c905ec82695beb83345453be2a
eastshine-high <eastshine.high@gmail.com>
2022-09-16 22:53:11 +0900

    test(user): 테스트용 Seller 생성 메소드 추가

--- app/src/test/java/com/eastshine/auction/user/UserFactory.java
+++ app/src/test/java/com/eastshine/auction/user/UserFactory.java
@@ -1,17 +1,23 @@
 package com.eastshine.auction.user;
 
+import com.eastshine.auction.user.application.SellerService;
 import com.eastshine.auction.user.application.UserService;
 import com.eastshine.auction.user.domain.User;
 import com.eastshine.auction.user.domain.UserRepository;
 import com.eastshine.auction.user.domain.role.RoleRepository;
+import com.eastshine.auction.user.domain.seller.Seller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
 import java.util.UUID;
 
 @Component
 public class UserFactory {
+    public static final Random RANDOM = new Random();
+
     @Autowired UserService userService;
+    @Autowired SellerService sellerService;
     @Autowired UserRepository userRepository;
     @Autowired RoleRepository roleRepository;
 
@@ -31,6 +37,23 @@ public class UserFactory {
                         .password(password)
                         .build()
         );
+    }
+
+    public Seller createSeller(String nickname) {
+        Seller seller = Seller.sellerBuilder()
+                .email(nickname + "@email.com")
+                .nickname(nickname)
+                .password(nickname)
+                .businessNumber(generateBusinessNumber())
+                .build();
+        return sellerService.signUpSeller(seller);
+    }
+
+    private String generateBusinessNumber() {
+        Long asLong = RANDOM.longs(1000000000L, 10000000000L)
+                .findFirst()
+                .getAsLong();
+        return asLong.toString();
     }
 
     public void deleteAllUser() {
