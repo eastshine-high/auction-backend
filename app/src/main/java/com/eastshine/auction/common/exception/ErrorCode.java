cfb055265362f5bd64e82ffbbebf630c3ab6704d
eastshine-high <eastshine.high@gmail.com>
2022-10-22 19:57:39 +0900

    feat(common): LOCK 실패 코드 추가

--- app/src/main/java/com/eastshine/auction/common/exception/ErrorCode.java
+++ app/src/main/java/com/eastshine/auction/common/exception/ErrorCode.java
@@ -13,24 +13,22 @@ public enum ErrorCode {
     COMMON_ILLEGAL_STATUS("잘못된 상태값입니다."),
     COMMON_INVALID_TOKEN("유효하지 않은 토큰입니다."),
     COMMON_UNAUTHORIZED_REQUEST("요청 권한이 없습니다."),
+    COMMON_LOCK_FAIL("LOCK 을 수행하는 중에 오류가 발생하였습니다."),
 
-    // Category
-    CATEGORY_PARENT_ENTITY_NOT_FOUND("상위 카테고리를 찾을 수 없습니다."),
-
-    // Item
-    ITEM_NOT_FOUND("물품을 찾을 수 없습니다."),
-    ITEM_OPTION_NOT_FOUND("물품 옵션을 찾을 수 없습니다."),
-    ITEM_INACCESSIBLE("물품에 대한 접근 권한이 없습니다."),
-    ITEM_STOCK_QUANTITY_ERROR("물품 재고를 확인해 주세요."),
-
-    // User
+    // user
     USER_DUPLICATE_EMAIL("이미 등록된 이메일입니다."),
     USER_DUPLICATE_NICKNAME("사용 중인 닉네임입니다."),
     USER_DUPLICATE_BUSINESS_NUMBER("등록된 사업자 번호입니다."),
     USER_NOT_FOUND("사용자를 찾을 수 없습니다."),
     USER_LOGIN_FAIL("아이디 또는 비밀번호를 잘못 입력했습니다."),
-    USER_INACCESSIBLE("사용자에 대한 접근 권한이 없습니다.");
+    USER_INACCESSIBLE("사용자에 대한 접근 권한이 없습니다."),
 
+    // product
+    CATEGORY_PARENT_ENTITY_NOT_FOUND("상위 카테고리를 찾을 수 없습니다."),
+    ITEM_NOT_FOUND("물품을 찾을 수 없습니다."),
+    ITEM_OPTION_NOT_FOUND("물품 옵션을 찾을 수 없습니다."),
+    ITEM_INACCESSIBLE("물품에 대한 접근 권한이 없습니다."),
+    ITEM_STOCK_QUANTITY_ERROR("물품 재고를 확인해 주세요.");
     private final String errorMsg;
 
     public String getErrorMsg(Object... arg) {
