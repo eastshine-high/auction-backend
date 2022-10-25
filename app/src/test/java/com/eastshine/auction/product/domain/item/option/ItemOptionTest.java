1cad3ff3be93b6b41194fbbceed033566bd6e979
eastshine-high <eastshine.high@gmail.com>
2022-10-25 22:53:16 +0900

    test(product): ItemOption - 테스트 추가

---
+++ app/src/test/java/com/eastshine/auction/product/domain/item/option/ItemOptionTest.java
@@ -0,0 +1,56 @@
+package com.eastshine.auction.product.domain.item.option;
+
+import com.eastshine.auction.common.exception.ErrorCode;
+import com.eastshine.auction.common.exception.InvalidArgumentException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ItemOptionTest {
+
+    @DisplayName("decreaseStockQuantity 메소드는")
+    @Nested
+    class Describe_decreaseStockQuantity{
+
+        @Nested
+        @DisplayName("보유한 재고 이상으로 재고를 차감하면")
+        class Context_with_invalid_quantity{
+            ItemOption itemOption = ItemOption.builder()
+                    .stockQuantity(1)
+                    .build();
+
+            @Test
+            @DisplayName("InvalidArgumentException 예외를 던진다.")
+            void it_throws_InvalidArgumentException() {
+                int invalidQuantity = 30;
+
+                assertThatThrownBy(() ->
+                        itemOption.decreaseStockQuantity(invalidQuantity)
+                )
+                        .isInstanceOf(InvalidArgumentException.class)
+                        .hasMessage(ErrorCode.ITEM_STOCK_QUANTITY_ERROR.getErrorMsg());
+            }
+        }
+
+        @Nested
+        @DisplayName("보유한 재고 이내의 재고를 차감하면")
+        class Context_with_valid_quantity{
+            ItemOption itemOption = ItemOption.builder()
+                    .stockQuantity(30)
+                    .build();
+
+            @Test
+            @DisplayName("재고를 감소시킨다.")
+            void it_decrease_stockQuantity() {
+                int validQuantity = 1;
+
+                itemOption.decreaseStockQuantity(validQuantity);
+                assertThat(itemOption.getStockQuantity())
+                        .isEqualTo(29);
+            }
+        }
+    }
+}
