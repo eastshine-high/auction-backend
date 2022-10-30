9aebc80d6ad60b1ff16a8dc3eec8b111a39ec846
eastshine-high <eastshine.high@gmail.com>
2022-10-30 21:44:21 +0900

    feat(config): RedissonClient 추가

--- app/src/main/java/com/eastshine/auction/config/EmbeddedRedisConfig.java
+++ app/src/main/java/com/eastshine/auction/config/EmbeddedRedisConfig.java
@@ -1,8 +1,14 @@
 package com.eastshine.auction.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import redis.embedded.RedisServer;
 
@@ -20,10 +26,12 @@ public class EmbeddedRedisConfig {
 
     private RedisServer redisServer;
 
+    @Bean
     @PostConstruct
-    public void redisServer() throws IOException {
+    public RedisServer redisServer() throws IOException {
         redisServer = new RedisServer(redisPort);
         redisServer.start();
+        return redisServer;
     }
 
     @PreDestroy
@@ -31,5 +39,15 @@ public class EmbeddedRedisConfig {
         if (redisServer != null) {
             redisServer.stop();
         }
+    }
+
+    @Bean
+    @Primary
+    @DependsOn("redisServer")
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://localhost:6379");
+        return Redisson.create(config);
     }
 }
