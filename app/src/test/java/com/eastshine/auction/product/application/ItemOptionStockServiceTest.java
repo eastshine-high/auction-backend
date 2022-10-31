9221ab146fcdbca4f2cebe4a0d996484514c5a55
eastshine-high <eastshine.high@gmail.com>
2022-10-31 22:57:22 +0900

    test(product): Github action ci 환경 과부화로 동시 요청 갯수 수정

--- app/src/test/java/com/eastshine/auction/product/application/ItemOptionStockServiceTest.java
+++ app/src/test/java/com/eastshine/auction/product/application/ItemOptionStockServiceTest.java
@@ -29,10 +29,10 @@ class ItemOptionStockServiceTest extends IntegrationTest {
     class Describe_decreaseStock{
 
         @Test
-        @DisplayName("5개의 재고의 동시 차감 요청을 처리할 수 있다.") // Github action 환경의 성능 문제로 테스트 동시 요청 최소화
+        @DisplayName("4개의 재고의 동시 차감 요청을 처리할 수 있다.") // Github action 환경의 성능 문제로 테스트 동시 요청 최소화
         void decreaseStockWithLock() throws InterruptedException {
             int stockQuantity = 400;
-            int concurrentConnectionCount = 5;
+            int concurrentConnectionCount = 4;
             ItemOption itemOption = ItemOption.builder()
                     .additionalPrice(9000)
                     .stockQuantity(stockQuantity)
