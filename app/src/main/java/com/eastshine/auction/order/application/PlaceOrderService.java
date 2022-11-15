e0187b7d5c644db41df09bb781d7fe922625a837
eastshine-high <eastshine.high@gmail.com>
2022-11-15 22:55:42 +0900

    refact(order): 네이밍 변경 OrderProducer -> PlaceOrderProducer

--- app/src/main/java/com/eastshine/auction/order/application/PlaceOrderService.java
+++ app/src/main/java/com/eastshine/auction/order/application/PlaceOrderService.java
@@ -1,6 +1,6 @@
 package com.eastshine.auction.order.application;
 
-import com.eastshine.auction.order.adaptor.OrderProducer;
+import com.eastshine.auction.order.adaptor.PlaceOrderProducer;
 import com.eastshine.auction.order.domain.Order;
 import com.eastshine.auction.order.web.dto.OrderDto;
 import com.eastshine.auction.product.application.ProductStockService;
@@ -13,14 +13,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class PlaceOrderService {
     private final ProductStockService productStockService;
     private final OrderService orderService;
-    private final OrderProducer orderProducer;
+    private final PlaceOrderProducer placeOrderProducer;
 
     @Transactional
     public Order placeOrder(OrderDto.PlaceOrderRequest request) {
         // Todo 1. 결제 검증 : '총 결제 금액'과 '총 물품 가격 합계'를 비교
-        request.getOrderItems().stream().forEach(productStockService::decreaseStock); // 2. 재고 차감
+        request.getOrderItems().stream()
+                .forEach(productStockService::decreaseStock); // 2. 재고 차감
         Order registeredOrder = orderService.registerOrder(request);// 3. 주문 등록
-        orderProducer.sendMail(request.getUserInfo(), registeredOrder); // 4. 메일 발송 이벤트 발행
+        placeOrderProducer.sendMail(request.getUserInfo(), registeredOrder); // 4. 메일 발송 이벤트 발행
         return registeredOrder;
     }
 }
