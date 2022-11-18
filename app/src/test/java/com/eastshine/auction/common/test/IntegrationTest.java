9e0f3c468f15336522eced5430255f376450d305
eastshine-high <eastshine.high@gmail.com>
2022-11-18 19:55:12 +0900

    feat(config): Embeded Config 설정

--- app/src/test/java/com/eastshine/auction/common/test/IntegrationTest.java
+++ app/src/test/java/com/eastshine/auction/common/test/IntegrationTest.java
@@ -1,15 +1,14 @@
 package com.eastshine.auction.common.test;
 
-import com.eastshine.auction.config.KafkaContainersConfig;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @AutoConfigureMockMvc
-@Import(KafkaContainersConfig.class)
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
 
