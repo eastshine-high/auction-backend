d77de7cb32b1813e43297799266ecd020fd7d56e
eastshine-high <eastshine.high@gmail.com>
2022-11-18 19:58:07 +0900

    feat(common): ErrorCode를 통한 예외 생성 추가

--- app/src/main/java/com/eastshine/auction/common/exception/IllegalStatusException.java
+++ app/src/main/java/com/eastshine/auction/common/exception/IllegalStatusException.java
@@ -7,6 +7,10 @@ public class IllegalStatusException extends BaseException {
         super(ErrorCode.COMMON_ILLEGAL_STATUS);
     }
 
+    public IllegalStatusException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
     public IllegalStatusException(String message) {
         super(message, ErrorCode.COMMON_ILLEGAL_STATUS);
     }
