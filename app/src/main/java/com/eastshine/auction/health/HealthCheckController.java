c8f1069a23737a84ebfa99a53029d99ad601aac8
eastshine-high <eastshine.high@gmail.com>
2022-12-17 19:55:12 +0900

    feat(health): HealthCheck 전용 컨트롤러

--- app/src/main/java/com/eastshine/auction/health/HealthCheckController.java
+++ app/src/main/java/com/eastshine/auction/health/HealthCheckController.java
@@ -1,7 +1,6 @@
 package com.eastshine.auction.health;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,17 +9,10 @@ import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @RestController
-public class CheckController {
-    @Value("${server.port}")
-    private int serverPort;
+public class HealthCheckController {
 
     @GetMapping("/api/health")
     public String healthCheck() {
-        return LocalDateTime.now().toString();
-    }
-
-    @GetMapping("/api/port")
-    public String portNumber() {
-        return String.valueOf(serverPort);
+        return "A server is on. At : " + LocalDateTime.now().toString();
     }
 }
